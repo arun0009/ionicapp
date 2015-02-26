a4dd5e8aa0baf81cce245bcce0608c747234bea2
Arun Gopalpuri <arun0009@gmail.com>
2015-02-26 14:18:06 -0800

    formatting

--- www/js/config/config.js
+++ www/js/config/config.js
@@ -6,7 +6,7 @@ angular.module("ionicapp")
 	"parseRestApiKey": "uPHtUvGK2kkjNMLCLj3H9CcfYzECr1X164wNWgrA",
 	"firebaseURL": "https://crackling-fire-4660.firebaseio.com",
 	"googleProjectNumber": "805566712685",
-    "googleTrackerId": "UA-59987837-1"
+    	"googleTrackerId": "UA-59987837-1"
 })
 
 ;
